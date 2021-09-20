29df29717468377b59e51ec54d296de77d85a591
Shumeiko <MShumeiko@luxoft.com>
2021-09-20 16:00:21 +0300

    Real GetEspresso

---
+++ VendingMachineConsole/VendingMachineConsole/GetCoffe/GetEspresso.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class GetEspresso
+{
+private:
+    /* data */
+public:
+    GetEspresso(/* args */);
+    ~GetEspresso();
+};
+
+GetEspresso::GetEspresso(/* args */)
+{
+}
+
+GetEspresso::~GetEspresso()
+{
+}
