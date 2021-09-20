bc201038ca0f7d64de57dddb6dde12a86aba6155
tommercoder <tommerstore@gmail.com>
2021-09-20 16:56:53 +0300

    [TS]

--- VendingMachineConsole/VendingMachineConsole/GetCoffe/GetEspresso.cpp
+++ VendingMachineConsole/VendingMachineConsole/GetCoffe/GetEspresso.cpp
@@ -7,19 +7,28 @@ class GetEspresso
 private:
     string coffe = "Coffe brrrr!";
     /* data */
+
     string GG;
+
     string EspressoName;
+
+    string EspressoName2;
 public:
     GetEspresso(/* args */);
+
     ~GetEspresso();
+
 
     void getCoffe(){
         cout << coffe<<" "<<EspressoName << endl;
     }
 };
 
+void DoSomething(){}
+
 GetEspresso::GetEspresso(/* args */)
 {
+    
 	GG = "default";
 
 
